7d3114d68153c70241dcb7f2866d5be69f3a0ca9
vanch <vanchkong@qq.com>
2023-01-06 21:00:44 +0800

    init

---
+++ docs/.vitepress/dist/assets/guide_theme-carbon-ads.md.7d48d0f9.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,a as e}from"./app.1f872c6b.js";const C=JSON.parse('{"title":"Carbon Ads","description":"","frontmatter":{},"headers":[],"relativePath":"guide/theme-carbon-ads.md","lastUpdated":null}'),o={name:"guide/theme-carbon-ads.md"},l=e("",6),p=[l];function t(r,c,i,D,d,y){return a(),n("div",null,p)}const A=s(o,[["render",t]]);export{C as __pageData,A as default};
---
+++ docs/.vitepress/dist/assets/guide_theme-footer.md.4ed2fa29.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,a as l}from"./app.1f872c6b.js";const A=JSON.parse('{"title":"Footer","description":"","frontmatter":{},"headers":[],"relativePath":"guide/theme-footer.md","lastUpdated":null}'),o={name:"guide/theme-footer.md"},p=l("",7),e=[p];function t(c,r,D,i,y,F){return a(),n("div",null,e)}const h=s(o,[["render",t]]);export{A as __pageData,h as default};
